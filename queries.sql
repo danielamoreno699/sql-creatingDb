6b7076834582d56dcc206d21d9daae6af69961f0
Daniela Moreno <dani9292@hotmail.com>
2023-06-05 15:50:21 -0500

    Write query for animales who have been neutered

--- queries.sql
+++ queries.sql
@@ -6,4 +6,6 @@ SELECT name FROM vet_clinic WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth
 FROM vet_clinic WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM vet_clinic WHERE weight > 10.5;
+SELECT * FROM vet_clinic WHERE neutered = true;
+
 
