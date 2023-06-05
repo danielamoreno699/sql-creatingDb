4e5c86f9763ee0afc1f0deb6a472dfd6c803ced1
Daniela Moreno <dani9292@hotmail.com>
2023-06-05 15:45:14 -0500

    Write query for finding birth animals

--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM vet_clinic WHERE name LIKE '%mon';
+SELECT name FROM vet_clinic WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
