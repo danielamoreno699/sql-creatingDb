d03f41706e959c9bdc9788527c56adcc0f940451
Daniela Moreno <dani9292@hotmail.com>
2023-06-05 15:49:06 -0500

    Write query for animales who have escaped with weith greater than 10.5

--- queries.sql
+++ queries.sql
@@ -5,4 +5,5 @@ SELECT name FROM vet_clinic WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-1
 SELECT name FROM vet_clinic WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth
 FROM vet_clinic WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM vet_clinic WHERE weight > 10.5;
 
