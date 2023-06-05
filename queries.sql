83d79789d03065c4824593dd708da557ed71c86a
Daniela Moreno <dani9292@hotmail.com>
2023-06-05 15:47:56 -0500

    Write query for animales whose name is pikachu and agumom

--- queries.sql
+++ queries.sql
@@ -3,4 +3,6 @@
 SELECT * FROM vet_clinic WHERE name LIKE '%mon';
 SELECT name FROM vet_clinic WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM vet_clinic WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth
+FROM vet_clinic WHERE name = 'Agumon' OR name = 'Pikachu';
 
