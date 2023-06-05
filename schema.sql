8cfa46eb44fc9df2944958e5df7d079aa447d472
Daniela Moreno <dani9292@hotmail.com>
2023-06-05 14:52:42 -0500

    Create table vet_clinic with data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE vet_clinic (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts int NOT NULL DEFAULT 0,
+    neurtered boolean NOT NULL DEFAULT false,
+    weight float NOT NULL DEFAULT 0,
 );
