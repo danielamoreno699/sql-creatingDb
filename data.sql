84055487d6381eecfd076b2c154fea86958014f2
Daniela Moreno <dani9292@hotmail.com>
2023-06-06 10:30:11 -0500

    Insert values animals

--- data.sql
+++ data.sql
@@ -10,3 +10,27 @@ VALUES
 
 /* Modify database schema, add specie column. */
 ALTER TABLE vet_clinic ADD COLUMN species varchar(255);
+
+/* Add more data */
+INSERT INTO vet_clinic (name, date_of_birth, escape_attempts, neutered, weight)
+VALUES ('Charmander', '2020-02-08', 0, false, -11);
+
+INSERT INTO vet_clinic (name, date_of_birth, escape_attempts, neutered, weight)
+VALUES ('Plantmon', '2021-11-15', 2, true, -5.7);
+
+INSERT INTO vet_clinic (name, date_of_birth, escape_attempts, neutered, weight)
+VALUES ('Squirtle', '1993-04-02', 3, false, -12.13);
+
+INSERT INTO vet_clinic (name, date_of_birth, escape_attempts, neutered, weight)
+VALUES ('Angemon', '2005-06-12', 1, true, -45);
+
+INSERT INTO vet_clinic (name, date_of_birth, escape_attempts, neutered, weight)
+VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
+
+INSERT INTO vet_clinic (name, date_of_birth, escape_attempts, neutered, weight)
+VALUES ('Blossom', '1998-10-13', 3, true, 17);
+
+INSERT INTO vet_clinic (name, date_of_birth, escape_attempts, neutered, weight)
+VALUES ('Ditto', '2022-05-14', 4, true, 22);
+
+
