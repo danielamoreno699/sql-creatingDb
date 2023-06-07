136cd3eff9fdb0e4c13c112ed05bda062b0fcf30
Daniela Moreno <dani9292@hotmail.com>
2023-06-07 09:39:23 -0500

    Create table named species

--- schema.sql
+++ schema.sql
@@ -13,9 +13,15 @@ CREATE TABLE vet_clinic (
 /* Modify database schema, add specie column. */
 ALTER TABLE vet_clinic ADD COLUMN species varchar(255);
 
-/* Insert new table named Owners */
+/* Create new table named Owners */
 CREATE TABLE owners (
     id SERIAL PRIMARY KEY,
     full_name varchar(100) NOT NULL,
     age int NOT NULL,
+);
+
+/* Create new table named species */
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL,
 );
